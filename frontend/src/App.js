9cd8a77a418d086befca53c71cc12603b353cabb
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2022-12-29 16:23:35 -0600

    Creating NotesList page to show all Notes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import NotesListPage from './pages/NotesListPage'
 
 function App() {
   return (
     <div className="App">
       <Header />
       Hello world!. This is my app
+      <NotesListPage />
     </div>
   );
 }
---
+++ frontend/src/pages/NotesListPage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NotesListPage() {
+    return (
+        <h2>hello from NotesListPage.js</h2>
+    )
+}
