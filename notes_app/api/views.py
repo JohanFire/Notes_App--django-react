3b44d99a99fd9ee9340ae2a077562b0fa18975ce
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2022-12-28 23:17:04 -0600

    renaming Endpoints

--- notes_app/api/views.py
+++ notes_app/api/views.py
@@ -5,31 +5,31 @@ from django.http import JsonResponse
 def get_routes(request):
     routes = [
         {
-            'Endpoint': '/notes/',
+            'Endpoint': '/api/notes/',
             'method': 'GET',
             'body': None,
             'description': 'Returns an array of notes'
         },
         {
-            'Endpoint': '/notes/id',
+            'Endpoint': '/api/notes/id',
             'method': 'GET',
             'body': None,
             'description': 'Returns a single note object'
         },
         {
-            'Endpoint': '/notes/create/',
+            'Endpoint': '/api/notes/create/',
             'method': 'POST',
             'body': {'body': ""},
             'description': 'Creates new note with data sent in post request'
         },
         {
-            'Endpoint': '/notes/id/update/',
+            'Endpoint': '/api/notes/id/update/',
             'method': 'PUT',
             'body': {'body': ""},
             'description': 'Creates an existing note with data sent in post request'
         },
         {
-            'Endpoint': '/notes/id/delete/',
+            'Endpoint': '/api/notes/id/delete/',
             'method': 'DELETE',
             'body': None,
             'description': 'Deletes and exiting note'
