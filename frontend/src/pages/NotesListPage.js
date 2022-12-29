befc8ea2b721f1f368d48b5e9c7f4b30555a87d5
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2022-12-29 16:34:38 -0600

    hooks

--- frontend/src/pages/NotesListPage.js
+++ frontend/src/pages/NotesListPage.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function NotesListPage() {
+    const [notes, setNotes] = useState([])
+
+    useEffect(() => {
+
+    }, [])
+
     return (
         <h2>hello from NotesListPage.js</h2>
     )
