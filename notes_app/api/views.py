9d9c94c25999290c12af5a6c019d7ebca992b04d
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2022-12-28 23:08:53 -0600

    first Route from api

--- notes_app/api/views.py
+++ notes_app/api/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+def get_routes(request):
+    # return JsonResponse({"routes": ["/api/notes/", "/api/notes/<str:pk>/"]}, safe=False)
+    return JsonResponse("our API", safe=False)
