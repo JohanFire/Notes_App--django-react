86da1c3b248e7719f71d5bfd408a8cbbe76d91ed
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2022-12-31 17:13:14 -0600

    Show description of each Note in HomePage

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -8,8 +8,20 @@ const get_title = (note) => {
 };
 
 const get_time = (note) => {
-    // return new Date(note.updated_at).toLocaleDateString()
-    return new Date(note.updated_at).toLocaleString()
+    return new Date(note.updated_at).toLocaleDateString()
+    // return new Date(note.updated_at).toLocaleString()
+};
+
+const get_content = (note) => {
+    const title = get_title(note)
+    let content = note.body.replaceAll('\n', ' ')
+    content = content.replaceAll(title, '')
+
+    if (content.length > 40) {
+        return content.slice(0, 40) + '...'
+    } else {
+        return content
+    }
 };
 
 const ListItem = ({ note }) => {
@@ -18,6 +30,7 @@ const ListItem = ({ note }) => {
             <div className='notes-list-item'>
                 <h3>{get_title(note)}</h3>
                 <p><span>{get_time(note)}</span></p>
+                <span>{get_content(note)}</span>
             </div>
         </Link>
     )
