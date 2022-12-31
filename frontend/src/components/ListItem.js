60cd005984907cf40eea42fb5b3db23b85244f0d
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2022-12-31 16:59:51 -0600

    Show date & time updated at of each note in Home page

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -7,11 +7,17 @@ const get_title = (note) => {
     return title.length > 40 ? title.substring(0, 40) + '...' : title
 };
 
+const get_time = (note) => {
+    // return new Date(note.updated_at).toLocaleDateString()
+    return new Date(note.updated_at).toLocaleString()
+};
+
 const ListItem = ({ note }) => {
     return (
         <Link to={`/note/${note.id}`}>
             <div className='notes-list-item'>
                 <h3>{get_title(note)}</h3>
+                <p><span>{get_time(note)}</span></p>
             </div>
         </Link>
     )
