f3a0a819b0e296245dc7d402536ee6343539a8b7
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2022-12-31 16:50:15 -0600

    Show only first 40 characters in Note's title in Home page

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+const get_title = (note) => {
+    const title = note.body.split('\n')[0]
+
+    return title.length > 40 ? title.substring(0, 40) + '...' : title
+};
+
 const ListItem = ({ note }) => {
     return (
         <Link to={`/note/${note.id}`}>
             <div className='notes-list-item'>
-                <h3>{note.body}</h3>
+                <h3>{get_title(note)}</h3>
             </div>
         </Link>
     )
